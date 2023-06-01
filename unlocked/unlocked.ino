715ac4ab4fbbd8c1da18dc61305eda1ecc451ef5
arshAIOT <77427347+arshAIOT@users.noreply.github.com>
2023-06-01 19:47:39 -0400

    Working Commit

--- unlocked/unlocked.ino
+++ unlocked/unlocked.ino
@@ -1,9 +1,13 @@
+//the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(2, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
